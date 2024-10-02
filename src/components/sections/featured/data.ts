4ab250e20df25e02075659c76a06001f647cf669
yukio <zviamontevilca@gmail.com>
2024-10-01 20:25:15 -0500

    moda e imagenes actualizadas
    imagenes y modal

--- src/components/sections/featured/data.ts
+++ src/components/sections/featured/data.ts
@@ -1,39 +1,43 @@
 import img1 from "./assets/solar-person.png";
 import img2 from "./assets/wind-person.png";
 import img3 from "./assets/amazonas2.png";
+import alvaro from "./assets/alvaro.jpg"
+import holgado from "./assets/holgado.jpg"
+import honorario from "./assets/honorio.jpg"
+import luis from "./assets/luis jose.jpg"
+import norma from "./assets/norma.jpg"
+import romario from "./assets/romario.jpg"
+import sillo from "./assets/sillo sosa.jpg"
 
 export const projectsSection = {
-  title: "Expositores",
-  subheading1_1: " Expositores ",
-  subheading1_2: " Nombrados ",
   projects: [
     {
       title: "ROMARIO DIAZ HOLGADO",
       description:
       "Ingeniero \n Impacto de la Inteligencia artificial en el Desarrollo de Software",
       location: "UNAMAD",
-      visual: img1,
+      visual: romario,
     },
     {
       title: "Álvaro Martín Aspilcueta Narváez.",
       description:
       "Proposal and Evaluation of a Software Architecture for a Smart Campus Digital Twin",
       location: "UNI",
-      visual: img2,
+      visual: alvaro,
     },
     {
       title: "Norma Catacora",
       description:
       "La inteligencia artificial en la mineria",
       location: "UNAJMA",
-      visual: img3,
+      visual: norma,
     },
     {
       title: "Luis José Díaz López",
       description:
       "Teledetección Satelital y sus Aplicaciones",
       location: "UNT",
-      visual: img3,
+      visual: luis,
     },
     {
       title: "Pablo César Alí Aruhuanca",
@@ -47,23 +51,21 @@ export const projectsSection = {
       description:
       "Exploración de predictores de la satisfacción con la vida de docentes peruanos a través de un ensamble de métodos de selección de características y aprendizaje automático",
       location: "UNAMAD",
-      visual: img3,
+      visual: holgado,
     },
     {
       title: "Jaffet Sillo Sosa",
       description:
       "Modelo de Inteligencia Artificial para la detección de Emociones",
       location: "UNAMAD",
-      visual: img3,
+      visual: sillo,
     },
     {
       title: "Honorio Apaza Alanoca",
       description:
       "NEURAL MACHINE TRANSLATION FOR NATIVE PERUVIAN LANGUAGES",
       location: "UNM",
-      visual: img3,
+      visual: honorario,
     }
   ],
-  callToAction: "Embark on a Green Energy Journey with [Company Name]",
-  button: "View More Projects",
 };
