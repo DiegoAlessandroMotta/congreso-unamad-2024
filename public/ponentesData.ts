389cf2706abb10702ae054cfd99c3cfe18d0e5cf
Luan-nee <luandelsol54@gmail.com>
2024-10-02 08:24:27 -0500

    mejorando los estilos del modal

---
+++ public/ponentesData.ts
@@ -0,0 +1,75 @@
+import img1 from "./assets/solar-person.png";
+import img2 from "./assets/wind-person.png";
+import img3 from "./assets/amazonas2.png";
+import alvaro from "./assets/alvaro.jpg";
+import holgado from "./assets/holgado.jpg";
+import honorario from "./assets/honorio.jpg";
+import luis from "./assets/luis jose.jpg";
+import norma from "./assets/norma.jpg";
+import romario from "./assets/romario.jpg";
+import sillo from "./assets/sillo sosa.jpg";
+
+export const projectsSection = {
+    projects: [
+        {
+            title: "ROMARIO DIAZ HOLGADO",
+            description:
+            "Ingeniero \n Impacto de la Inteligencia artificial en el Desarrollo de Software",
+            location: "UNAMAD",
+            visual: romario,
+            biografia: "",
+        },
+        {
+            title: "Álvaro Martín Aspilcueta Narváez.",
+            description:
+            "Proposal and Evaluation of a Software Architecture for a Smart Campus Digital Twin",
+            location: "UNI",
+            visual: alvaro,
+            biografia: "",
+        },
+        {
+            title: "Norma Catacora",
+            description: "La inteligencia artificial en la mineria",
+            location: "UNAJMA",
+            visual: norma,
+            biografia: "",
+        },
+        {
+            title: "Luis José Díaz López",
+            description: "Teledetección Satelital y sus Aplicaciones",
+            location: "UNT",
+            visual: luis,
+            biografia: "",
+        },
+        {
+            title: "Pablo César Alí Aruhuanca",
+            description: "Metodología Ágiles",
+            location: "UNAJMA",
+            visual: img3,
+            biografia: "",
+        },
+        {
+            title: "Luis Alberto Holgado Apaza",
+            description:
+            "Exploración de predictores de la satisfacción con la vida de docentes peruanos a través de un ensamble de métodos de selección de características y aprendizaje automático",
+            location: "UNAMAD",
+            visual: holgado,
+            biografia: "",
+        },
+        {
+            title: "Jaffet Sillo Sosa",
+            description:
+            "Modelo de Inteligencia Artificial para la detección de Emociones",
+            location: "UNAMAD",
+            visual: sillo,
+            biografia: "",
+        },
+        {
+            title: "Honorio Apaza Alanoca",
+            description: "NEURAL MACHINE TRANSLATION FOR NATIVE PERUVIAN LANGUAGES",
+            location: "UNM",
+            visual: honorario,
+            biografia: "",
+        },
+    ],
+};
