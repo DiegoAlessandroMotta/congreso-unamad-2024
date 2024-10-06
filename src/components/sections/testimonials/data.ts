712c045439f85eb6439eb8c9c4f97268d71bdc99
Luan-nee <luandelsol54@gmail.com>
2024-10-05 20:13:39 -0500

    agregando patrocinadores y redes sociales de la universidad

--- src/components/sections/testimonials/data.ts
+++ src/components/sections/testimonials/data.ts
@@ -1,7 +1,21 @@
-import apolo from "./assets/apolo tech.jpeg"
-import odonto from "./assets/odonto esthetic.png"
-import toreto from "./assets/toreto gym.jpeg"
-import iveett from "./assets/iveet sac.jpg"
+import img1 from './assets/Centro-de-innovacion-tecnologica.webp'
+import img2 from './assets/Iotic.webp'
+import img3 from './assets/adl-system-tecnology.jpg'
+import img4 from './assets/apolo-tech.webp'
+import img5 from './assets/avidetalles.webp'
+import img6 from './assets/cjava.webp'
+import img7 from './assets/codicore-sac.webp'
+import img8 from './assets/dental-family.webp'
+import img9 from './assets/dental-plus.webp'
+import img10 from './assets/eco-house-lazos.webp'
+import img11 from './assets/hotel-la-torre.webp'
+import img12 from './assets/iveet-sac.webp'
+import img13 from './assets/mundo-click.webp'
+import img14 from './assets/odonto-esthetic.webp'
+import img15 from './assets/pcsoluciones.webp'
+import img16 from './assets/satra.webp'
+import img17 from './assets/toreto-gym.webp'
+import img18 from './assets/hotel-el-huerto.png'
 
 export const testimonialsSection = {
   title: "Patrocinadores",
@@ -9,24 +23,112 @@ export const testimonialsSection = {
   subheading1_2: " PATROCINADORES ",
   testimonials: [
     {
-      name: "ODONTO ESTHETIC",
-      visual: odonto,
-      direccion:""
+      name: "Centro-de-innovacion-tecnologica",
+      logo: img1,
+      direccion:"#",
+      styleRedondear: "bg-white rounded-md"
     },
     {
-      name: "APOLO TECH",
-      visual: apolo,
-      direccion:""
+      name: "Iotic",
+      logo: img2,
+      direccion:"#",
+      styleRedondear: "rounded-md"
     },
     {
-      name: "TORETO GYM",
-      visual: toreto,
-      direccion:""
+      name: "adl-system-tecnology",
+      logo: img3,
+      direccion:"#",
+      styleRedondear: "rounded-md"
     },
     {
-      name: "IVETT SAC",
-      visual: iveett,
-      direccion:""
+      name: "apolo-tech",
+      logo: img4,
+      direccion:"#",
+      styleRedondear: "rounded-full"
+    },
+    {
+      name: "avidetalles",
+      logo: img5,
+      direccion:"#",
+      styleRedondear: "rounded-full"
+    },
+    {
+      name: "cjava",
+      logo: img6,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "codicore-sac",
+      logo: img7,
+      direccion:"#",
+      styleRedondear: "bg-white rounded-md"
+    },
+    {
+      name: "dental-family",
+      logo: img8,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "dental-plus",
+      logo: img9,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "eco-house-lazos",
+      logo: img10,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "hotel-la-torre",
+      logo: img11,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "el-huerto",
+      logo: img18,
+      direccion: "#",
+      styleRedondear: "rounded-full"
+    },
+    {
+      name: "iveet-sac",
+      logo: img12,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "mundo-click",
+      logo: img13,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "odonto-esthetic",
+      logo: img14,
+      direccion:"#",
+      styleRedondear: "rounded-md"
+    },
+    {
+      name: "pcsoluciones",
+      logo: img15,
+      direccion:"#",
+      styleRedondear: "bg-white rounded-md"
+    },
+    {
+      name: "satra",
+      logo: img16,
+      direccion:"#",
+      styleRedondear: "bg-white rounded-md"
+    },
+    {
+      name: "toreto-gym",
+      logo: img17,
+      direccion:"#",
+      styleRedondear: "rounded-md"
     }
   ]
 };
