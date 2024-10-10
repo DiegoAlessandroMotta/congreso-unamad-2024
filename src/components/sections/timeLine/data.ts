158e9cf074de338f5283a0bab1cb811b495e2113
Luan-nee <luandelsol54@gmail.com>
2024-10-10 06:17:18 -0500

    modificando los datos del cronograma

--- src/components/sections/timeLine/data.ts
+++ src/components/sections/timeLine/data.ts
@@ -3,95 +3,146 @@ import danielAliaga from "./assets/daniel-aliaga.jpg"
 import darioVieira from "./assets/dario-vieira.jpg"
 import rickRoberts from "./assets/rick-roberts.jpg"
 
+// LOS DATOS YA ESTÁN ACTUALIZADOS excepto la foto de cada ponente
+
 export const data = [
   [
     {
-      tituloPonencia: "Impacto de la Inteligencia artificial en el Desarrollo de Software",
-      nombrePonente: "Romario Diaz Holgado",
-      horario: "10:00 - 13:00",
+      tituloPonencia: "IOT EN EL CONTEXTO DE LAS CIUDADES INTELIGENTES Y EL DESARROLLO REGIONAL",
+      nombrePonente: "DIODOMIRO BALDOMERO LUQUE CARCASI",
+      horario: "10:00 - 11:00",
       foto: arlindoConceicao,
-      descripFoto: "foto del ponente Romario Diaz Holgado"
+      descripFoto: "foto del ponente DIODOMIRO BALDOMERO LUQUE CARCASI"
     },
     {
-      tituloPonencia: "Proposal and Evaluation of a Software Architecture for a Smart Campus Digital Twin",
-      nombrePonente: "Álvaro Martín Aspilcueta Narváez.",
-      horario: "13:00 - 14:00",
+      tituloPonencia: "( vacío )",
+      nombrePonente: "ANTONIO PUMA CHOQUEHUANCA",
+      horario: "11:00 - 12:00",
       foto: danielAliaga,
       descripFoto: "foto del ponente Álvaro Martín Aspilcueta Narváez"
     },
     {
-      tituloPonencia: "La inteligencia artificial en la mineria",
-      nombrePonente: "Norma Catacora",
-      horario: "14:00 - 15:00",
+      tituloPonencia: "INGENIERÍA DE SOFTWARE: TENDENCIAS DE LAS METODOLOGÍAS ACTUALES",
+      nombrePonente: "DANY DORIAN ISUIZA PEREZ",
+      horario: "12:00 - 13:00",
       foto: darioVieira,
-      descripFoto: "foto del ponente Norma Catacora"
+      descripFoto: "foto del ponente DANY DORIAN ISUIZA PEREZ"
     },
     {
-      tituloPonencia: "Teledetección Satelital y sus Aplicaciones",
-      nombrePonente: "Luis José Díaz López",
+      tituloPonencia: "INFRAESTRUCTURA DE DATOS ESPACIALES Y CATASTRO ÚNICO REGIONAL",
+      nombrePonente: "JOSE CARLOS NAVARRO VEGA",
       horario: "15:00 - 16:00",
       foto: rickRoberts,
-      descripFoto: "foto del ponente Luis José Díaz López"
+      descripFoto: "foto del ponente JOSE CARLOS NAVARRO VEGA"
+    },
+    {
+      tituloPonencia: "OS DESAFIOS DO ENSINO, PESQUISA E EXTENSÃO DA CIÊNCIA DA COMPUTAÇÃO NA AMAZÔNIA",
+      nombrePonente: "ANSELMO FORTUNATO RUIZ RODRIGUEZ",
+      horario: "16:00 - 17:00",
+      foto: rickRoberts,
+      descripFoto: "foto del ponente ANSELMO FORTUNATO RUIZ RODRIGUEZ"
+    },
+    {
+      tituloPonencia: "MODELO DE INTELIGENCIA ARTIFICIAL PARA LA DETECCIÓN DE EMOCIONES",
+      nombrePonente: "JAFFET SILLO SOSA",
+      horario: "17:00 - 18:00",
+      foto: rickRoberts,
+      descripFoto: "foto del ponente JAFFET SILLO SOSA"
+    },
+    {
+      tituloPonencia: "( vacío )",
+      nombrePonente: "( vacío )",
+      horario: "18:00 - 19:00",
+      foto: rickRoberts,
+      descripFoto: "( vacío )"
     }
   ],
   [
     {
-      "tituloPonencia": "Transformación Digital en las Empresas",
-      "nombrePonente": "Roberto Mendez",
-      "horario": "8:00 - 9:00",
-      "foto": arlindoConceicao,
-      "descripFoto": "Foto del ponente Roberto Mendez"
+      tituloPonencia: "ANÁLISIS BIG DATA EN LOS PROCESOS ACADÉMICOS DE LA UNIVERSIDAD AMAZÓNICA DE PANDO (UAP)",
+      nombrePonente: "JUAN CARLOS HUANCA GUANCA",
+      horario: "9:00 - 10:00",
+      foto: arlindoConceicao,
+      descripFoto: "Foto del ponente JUAN CARLOS HUANCA GUANCA"
     },
     {
-      "tituloPonencia": "Ciberseguridad en la Era de los Datos",
-      "nombrePonente": "Ana Beatriz Salinas",
-      "horario": "10:00 - 12:00",
-      "foto": danielAliaga,
-      "descripFoto": "Foto del ponente Ana Beatriz Salinas"
+      tituloPonencia: "DETECCIÓN DEL CELO EN VACUNO DE CARNE USANDO IOT Y APRENDIZAJE AUTOMATICO",
+      nombrePonente: "JAIME PRIETO LUNA",
+      horario: "10:00 - 11:00",
+      foto: danielAliaga,
+      descripFoto: "Foto del ponente JAIME PRIETO LUNA"
     },
     {
-      "tituloPonencia": "Desarrollo Sostenible y Tecnología",
-      "nombrePonente": "Carlos Mendoza",
-      "horario": "14:00 - 15:00",
-      "foto": darioVieira,
-      "descripFoto": "Foto del ponente Carlos Mendoza"
+      tituloPonencia: "INTELIGENCIA ARTIFICIAL Y EDUCACIÓN UNIVERSITARIA",
+      nombrePonente: "PONCE ATENCIO YALMAR TEMISTOCLES",
+      horario: "11:00 - 12:00",
+      foto: darioVieira,
+      descripFoto: "Foto del ponente PONCE ATENCIO YALMAR TEMISTOCLES"
     },
     {
-      "tituloPonencia": "Blockchain y el Futuro de las Finanzas",
-      "nombrePonente": "María Fernanda Rodríguez",
-      "horario": "17:00 - 19:00",
-      "foto": rickRoberts,
-      "descripFoto": "Foto del ponente María Fernanda Rodríguez"
+      tituloPonencia: "IMPACTO DE LA INTELIGENCIA ARTIFICIAL EN EL DESARROLLO DE SOFTWARE",
+      nombrePonente: "ROMARIO DIAZ HOLGADO",
+      horario: "12:00 - 13:00",
+      foto: rickRoberts,
+      descripFoto: "Foto del ponente ROMARIO DIAZ HOLGADO"
+    },
+    {
+      tituloPonencia: "( vacío )",
+      nombrePonente: "RENE YANA CHOQUE",
+      horario: "15:00 - 16:00",
+      foto: rickRoberts,
+      descripFoto: "Foto del ponente RENE YANA CHOQUE"
+    },
+    {
+      tituloPonencia: "EXPLORACIÓN DE PREDICTORES DE LA SATISFACCIÓN CON LA VIDA DE DOCENTES PERUANOS MEDIANTE UN ENSAMBLE DE MÉTODOS DE SELECCIÓN DE CARACTERÍSTICAS",
+      nombrePonente: "LUIS ALBERTO HOLGADO APAZA",
+      horario: "16:00 - 17:00",
+      foto: rickRoberts,
+      descripFoto: "Foto del ponente LUIS ALBERTO HOLGADO APAZA"
+    },
+    {
+      tituloPonencia: "( vacío )",
+      nombrePonente: "EDY LARICO MAMANI",
+      horario: "17:00 - 18:00",
+      foto: rickRoberts,
+      descripFoto: "Foto del ponente ROMARIO DIAZ HOLGADO"
+    },
+    {
+      tituloPonencia: "( vacío )",
+      nombrePonente: "( vacío )",
+      horario: "( vacío )",
+      foto: rickRoberts,
+      descripFoto: "( vacío )"
     }
   ],
   [
     {
-      "tituloPonencia": "Impacto de la Inteligencia Artificial en el Desarrollo de Software",
-      "nombrePonente": "Romario Diaz Holgado",
-      "horario": "10:00 - 13:00",
-      "foto": arlindoConceicao,
-      "descripFoto": "Foto del ponente Romario Diaz Holgado"
+      tituloPonencia: "INTELIGENCIA ARTIFICIAL GENERATIVA PARA CREACIÓN DE CONTENIDO VISUAL EN VISIÓN COMPUTACIONAL",
+      nombrePonente: "ANA BEATRIZ ALVAREZ MAMANI",
+      horario: "9:00 - 10:00",
+      foto: arlindoConceicao,
+      descripFoto: "Foto del ponente ANA BEATRIZ ALVAREZ MAMANI"
     },
     {
-      "tituloPonencia": "Proposal and Evaluation of a Software Architecture for a Smart Campus Digital Twin",
-      "nombrePonente": "Álvaro Martín Aspilcueta Narváez",
-      "horario": "13:00 - 14:00",
-      "foto": danielAliaga,
-      "descripFoto": "Foto del ponente Álvaro Martín Aspilcueta Narváez"
+      tituloPonencia: "APLICACIONES DE LA INTELIGENCIA ARTIFICIAL EN LA MINERÍA CON OPERACIONES DE TAJO ABIERTO: UN ENFOQUE SISTÉMICO",
+      nombrePonente: "NORMA CATACORA FLORES",
+      horario: "10:00 - 11:00",
+      foto: danielAliaga,
+      descripFoto: "Foto del ponente NORMA CATACORA FLORES"
     },
     {
-      "tituloPonencia": "La Inteligencia Artificial en la Minería",
-      "nombrePonente": "Norma Catacora",
-      "horario": "14:00 - 15:00",
-      "foto": darioVieira,
-      "descripFoto": "Foto del ponente Norma Catacora"
+      tituloPonencia: "TELEDETECCIÓN SATELITAL Y SUS APLICACIONES",
+      nombrePonente: "LUIS JOSÉ DÍAZ LÓPEZ",
+      horario: "11:00 - 12:00",
+      foto: darioVieira,
+      descripFoto: "Foto del ponente LUIS JOSÉ DÍAZ LÓPEZ"
     },
     {
-      "tituloPonencia": "Teledetección Satelital y sus Aplicaciones",
-      "nombrePonente": "Luis José Díaz López",
-      "horario": "15:00 - 16:00",
-      "foto": rickRoberts,
-      "descripFoto": "Foto del ponente Luis José Díaz López"
+      tituloPonencia: "MODELOS GENERATIVOS MULTIMODALES",
+      nombrePonente: "CESAR ARMANDO BELTRAN CASTAÑON",
+      horario: "12:00 - 13:00",
+      foto: rickRoberts,
+      descripFoto: "Foto del ponente CESAR ARMANDO BELTRAN CASTAÑON"
     }
   ]
 ]
