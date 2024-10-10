558465184effd1de63bf25bd90f9cfc81241b667
yukio <zviamontevilca@gmail.com>
2024-10-10 09:16:37 -0500

    se creo la data 2
    esson los datos del cronograma

---
+++ src/components/sections/timeLine/data2.ts
@@ -0,0 +1,268 @@
+import arlindoConceicao from "./assets/arlindo-conceicao.jpg"
+import danielAliaga from "./assets/daniel-aliaga.jpg"
+import darioVieira from "./assets/dario-vieira.jpg"
+import rickRoberts from "./assets/rick-roberts.jpg"
+
+// LOS DATOS YA ESTÁN ACTUALIZADOS excepto la foto de cada ponente
+
+export const data = 
+  
+    {
+      "dia1":[
+        {
+          "grado":"doctor",
+          "nombre":"Diodomio Baldomero Luque Carcasi",
+          "universidad":"Universidade Federal do Acre",
+          "U":"UFDA",
+          "pais":"brasil",
+          "tema":"IOT EN EL CONTEXTO DE LAS CIUDADES INTELIGENTES Y EL DESARROLLO REGIONAL",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"10.00am",
+              "hora_final":"11.00am"
+            }
+          
+        },{
+          "grado":"Ingeniero",
+          "nombre":"Antonio Poma Choquehuanca",
+          "universidad":"Universidad Amazónica de Pando",
+          "U":"UAP",
+          "pais":"Bolivia",
+          "tema":"",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"11.00am",
+              "hora_final":"12.00am"
+            }
+        },{
+          "grado":"Ingeniero",
+          "nombre":"DANY DORIAN ISUIZA PEREZ",
+          "universidad":"Universidad Nacional Amazónica de Madre de Dios",
+          "U":"UNAMAD",
+          "pais":"Perú",
+          "tema":"INGENIERÍA DE SOFTWARE: TENDENCIAS DE LAS METODOLOGÍAS ACTUALES",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"12.00am",
+              "hora_final":"1.00pm"
+            }
+        },
+        {
+          "grado":"Doctor",
+          "nombre":"JOSE CARLOS NAVARRO VEGA",
+          "universidad":"Gobierno Regional de Madre de Dios",
+          "U":"GRMD",
+          "pais":"Perú",
+          "tema":"INFRAESTRUCTURA DE DATOS ESPACIALES Y CATASTRO ÚNICO REGIONAL",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"15.00Pm",
+              "hora_final":"16.00pm"
+            }
+        },{
+          "grado":"Doctor",
+          "nombre":"ANSELMO FORTUNATO RUIZ RODRIGUEZ",
+          "universidad":"UNIVERSIDAD FEDERAL DE ACRE",
+          "U":"UFA",
+          "pais":"Brasil",
+          "tema":"LOS DESAFÍOS DE LA ENSEÑANZA, INVESTIGACIÓN Y EXTENSIÓN DE LA INFORMÁTICA EN LA AMAZONIA",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"16.00Pm",
+              "hora_final":"17.00pm"
+            }
+        },{
+          "grado":"M.Sc.",
+          "nombre":"JAFFET SILLO SOSA",
+          "universidad":"Universidad Nacional Amazónica de Madre de Dios",
+          "U":"UNAMAD",
+          "pais":"Perú",
+          "tema":"MODELO DE INTELIGENCIA ARTIFICIAL PARA LA DETECCIÓN DE EMOCIONES",
+          "cronograma":
+            {
+              "dia":"miercoles",
+              "fecha":"16 de octubre 2024",
+              "hora_inicio":"17.00Pm",
+              "hora_final":"18.00pm"
+            }
+        }
+      ],
+      "dia2":[
+        {
+          "grado":"Magister",
+          "nombre":"JUAN CARLOS HUANCA GUANCA",
+          "universidad":"UNIVERSIDAD AMAZÓNICA DE PANDO",
+          "U":"UAP",
+          "pais":"Bolivia",
+          "tema":"ANÁLISIS BIG DATA EN LOS PROCESOS ACADÉMICOS DE LA UNIVERSIDAD AMAZÓNICA DE PANDO",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"9.00am",
+              "hora_final":"10.00am"
+            }
+        },
+        {
+          "grado":"M.Sc.",
+          "nombre":"JAIME PRIETO LUNA",
+          "universidad":"Universidad Nacional Amazónica de Madre de Dios",
+          "U":"UNAMAD",
+          "pais":"Perú",
+          "tema":"DETECCIÓN DEL CELO EN VACUNO DE CARNE USANDO IOT Y APRENDIZAJE AUTOMATICO",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"10.00am",
+              "hora_final":"11.00am"
+            }
+        },
+        {
+          "grado":"Doctor",
+          "nombre":"PONCE ATENCIO YALMAR TEMISTOCLES",
+          "universidad":"UNIVERSIDAD NACIONAL DE JULIACA",
+          "U":"UNAJ",
+          "pais":"Perú",
+          "tema":"DETECCIÓN DEL CELO EN VACUNO DE CARNE USANDO IOT Y APRENDIZAJE AUTOMATICO",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"11.00am",
+              "hora_final":"12.00am"
+            }
+        },{
+          "grado":"Ingeniero",
+          "nombre":"ROMARIO DIAZ HOLGADO",
+          "universidad":"UNIVERSIDAD NACIONAL AMAZONICA DE MADRE DE DIOS",
+          "U":"UNAMAD",
+          "pais":"Perú",
+          "tema":"IMPACTO DE LA INTELIGENCIA ARTIFICIAL EN EL DESARROLLO DE SOFTWARE",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"12.00am",
+              "hora_final":"1.00Pm"
+            }
+        },
+        {
+          "grado":"Ingeniero",
+          "nombre":"RENE EMIGDIO YANA CHOQUE",
+          "universidad":"UNIVERSIDAD AMAZÓNICA DE PANDO",
+          "U":"UAP",
+          "pais":"Bolivia",
+          "tema":"",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"15.00pm",
+              "hora_final":"16.00Pm"
+            }
+        },
+        {
+          "grado":"Doctor",
+          "nombre":"LUIS ALBERTO HOLGADO APAZA",
+          "universidad":"Universidad Nacional Amazónica de Madre de Dios",
+          "U":"UNAMAD",
+          "pais":"Perú",
+          "tema":"EXPLORACIÓN DE PREDICTORES DE LA SATISFACCIÓN CON LA VIDA DE DOCENTES PERUANOS MEDIANTE UN ENSAMBLE DE MÉTODOS DE SELECCIÓN DE CARACTERÍSTICAS",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"16.00pm",
+              "hora_final":"17.00Pm"
+            }
+        },
+        {
+          "grado":"Doctor",
+          "nombre":"EDY LARICO MAMANI",
+          "universidad":"UNIVERSIDAD NACIONAL DE JULIACA",
+          "U":"UNAJ",
+          "pais":"Perú",
+          "tema":"",
+          "cronograma":
+            {
+              "dia":"jueves",
+              "fecha":"17 de octubre 2024",
+              "hora_inicio":"17.00pm",
+              "hora_final":"18.00Pm"
+            }
+        }
+      ],
+      "dia3":[
+        {
+          "grado":"Doctora",
+          "nombre":"ANA BEATRIZ ALVAREZ MAMANI",
+          "universidad":"UNIVERSIDAD FEDERAL DE ACRE",
+          "U":"UFA",
+          "pais":"Brasil",
+          "tema":"INTELIGENCIA ARTIFICIAL GENERATIVA PARA CREACIÓN DE CONTENIDO VISUAL EN VISIÓN COMPUTACIONAL",
+          "cronograma":
+            {
+              "dia":"viernes",
+              "fecha":"18 de octubre 2024",
+              "hora_inicio":"9.00am",
+              "hora_final":"10.00am"
+            }
+        },
+        {
+          "grado":"Doctora",
+          "nombre":"NORMA LORENA CATACORA FLORES",
+          "universidad":"UNIVERSIDAD NACIONAL JOSE MARIA ARGUEDAS",
+          "U":"UNJMA",
+          "pais":"Peru",
+          "tema":"APLICACIONES DE LA INTELIGENCIA ARTIFICIAL EN LA MINERÍA CON OPERACIONES DE TAJO ABIERTO: UN ENFOQUE SISTÉMICO",
+          "cronograma":
+            {
+              "dia":"viernes",
+              "fecha":"18 de octubre 2024",
+              "hora_inicio":"10.00am",
+              "hora_final":"11.00am"
+            }
+        },{
+          "grado":"Ingeniero",
+          "nombre":"LUIS JOSÉ DÍAZ LÓPEZ",
+          "universidad":"Universidad Tecnológica de Trujillo",
+          "U":"UTP",
+          "pais":"Perú",
+          "tema":"TELEDETECCIÓN SATELITAL Y SUS APLICACIONES",
+          "cronograma":
+            {
+              "dia":"viernes",
+              "fecha":"18 de octubre 2024",
+              "hora_inicio":"11.00am",
+              "hora_final":"12.00Pm"
+            }
+        },
+        {
+          "grado":"Doctor",
+          "nombre":"CESAR ARMANDO BELTRAN CASTAÑON",
+          "universidad":"PONTIFICIA UNIVERSIDAD CATÓLICA DEL PERÚ",
+          "U":"PUCP",
+          "pais":"Perú",
+          "tema":"MODELOS GENERATIVOS MULTIMODALES",
+          "cronograma":
+            {
+              "dia":"viernes",
+              "fecha":"18 de octubre 2024",
+              "hora_inicio":"12.00am",
+              "hora_final":"1.00Pm"
+            }
+        }
+      ]
+    }
+  
