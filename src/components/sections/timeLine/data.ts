a2230cb3140f5a197ffe079802234c14bd26aad5
Luan-nee <luandelsol54@gmail.com>
2024-10-10 14:07:45 -0500

    agregando cambios en la linea de tiempo

--- src/components/sections/timeLine/data.ts
+++ src/components/sections/timeLine/data.ts
@@ -1,8 +1,20 @@
-import arlindoConceicao from "./assets/arlindo-conceicao.jpg"
-import danielAliaga from "./assets/daniel-aliaga.jpg"
-import darioVieira from "./assets/dario-vieira.jpg"
-import rickRoberts from "./assets/rick-roberts.jpg"
-
+import diodomiro from "./assets/biodomio baldomero luque.png"
+import antonio from "./assets/marco antonio poma choquehuanca.png"
+import dany from "./assets/dany doriam.webp"
+import joseCarlos from "./assets/jose carlos.jpg"
+import anselmo from "./assets/anselmo rodriguez.webp"
+import silloSosa from "./assets/sillo sosa.webp"
+import JuanCarlos from "./assets/juan carlos huanca guanca.png"
+import jaime from "./assets/jaime cesar prieto.webp"
+import ponce from "./assets/yalmar temistoqles.webp"
+import romario from "./assets/romario diaz.webp"
+import rene from "./assets/rene.png"
+import holgado from "./assets/luis alberto.webp"
+import edy from "./assets/edy.jpg"
+import ana from "./assets/ana.png"
+import norma from "./assets/norma lorena.webp";
+import luis from "./assets/luis jose.jpg";
+import cesar from "./assets/cesar.jpeg"
 // LOS DATOS YA ESTÁN ACTUALIZADOS excepto la foto de cada ponente
 
 export const data = [
@@ -11,42 +23,42 @@ export const data = [
       tituloPonencia: "Iot En El Contexto De Las Ciudades Inteligentes Y El Desarrollo Regional",
       nombrePonente: "Diodomiro Baldomero Luque Carcasi",
       horario: "10:00 - 11:00",
-      foto: arlindoConceicao,
+      foto: diodomiro,
       descripFoto: "foto del ponente DIODOMIRO BALDOMERO LUQUE CARCASI"
     },
     {
       tituloPonencia: "( vacío )",
       nombrePonente: "Antonio Puma Choquehuanca",
       horario: "11:00 - 12:00",
-      foto: danielAliaga,
+      foto: antonio,
       descripFoto: "foto del ponente Álvaro Martín Aspilcueta Narváez"
     },
     {
       tituloPonencia: "Ingeniería De Software: Tendencias De Las Metodologías Actuales",
       nombrePonente: "Dany Dorian Isuiza Perez",
       horario: "12:00 - 13:00",
-      foto: darioVieira,
+      foto: dany,
       descripFoto: "foto del ponente DANY DORIAN ISUIZA PEREZ"
     },
     {
       tituloPonencia: "Infraestructura De Datos Espaciales Y Catastro Único Regional",
       nombrePonente: "Jose Carlos Navarro Vega",
       horario: "15:00 - 16:00",
-      foto: rickRoberts,
+      foto: joseCarlos,
       descripFoto: "foto del ponente JOSE CARLOS NAVARRO VEGA"
     },
     {
       tituloPonencia: "Os Desafios Do Ensino, Pesquisa E Extensão Da Ciência Da Computação Na Amazônia",
       nombrePonente: "Anselmo Fortunato Ruiz Rodriguez",
       horario: "16:00 - 17:00",
-      foto: rickRoberts,
+      foto: anselmo,
       descripFoto: "foto del ponente ANSELMO FORTUNATO RUIZ RODRIGUEZ"
     },
     {
       tituloPonencia: "Modelo De Inteligencia Artificial Para La Detección De Emociones",
       nombrePonente: "Jaffet Sillo Sosa",
       horario: "17:00 - 18:00",
-      foto: rickRoberts,
+      foto: silloSosa,
       descripFoto: "foto del ponente JAFFET SILLO SOSA"
     }
   ],
@@ -55,49 +67,49 @@ export const data = [
       tituloPonencia: "Análisis Big Data En Los Procesos Académicos De La Universidad Amazónica De Pando (Uap)",
       nombrePonente: "Juan Carlos Huanca Guanca",
       horario: "9:00 - 10:00",
-      foto: arlindoConceicao,
+      foto: JuanCarlos,
       descripFoto: "Foto del ponente JUAN CARLOS HUANCA GUANCA"
     },
     {
       tituloPonencia: "Detección Del Celo En Vacuno De Carne Usando Iot Y Aprendizaje Automatico",
-      nombrePonente: "Jaime Prieto Luna",
+      nombrePonente: "Jaime César Prieto Luna",
       horario: "10:00 - 11:00",
-      foto: danielAliaga,
+      foto: jaime,
       descripFoto: "Foto del ponente JAIME PRIETO LUNA"
     },
     {
       tituloPonencia: "Inteligencia Artificial Y Educación Universitaria",
       nombrePonente: "Ponce Atencio Yalmar Temistocles",
       horario: "11:00 - 12:00",
-      foto: darioVieira,
+      foto: ponce,
       descripFoto: "Foto del ponente PONCE ATENCIO YALMAR TEMISTOCLES"
     },
     {
       tituloPonencia: "Impacto De La Inteligencia Artificial En El Desarrollo De Software",
       nombrePonente: "Romario Diaz Holgado",
       horario: "12:00 - 13:00",
-      foto: rickRoberts,
+      foto: romario,
       descripFoto: "Foto del ponente ROMARIO DIAZ HOLGADO"
     },
     {
       tituloPonencia: "( vacío )",
       nombrePonente: "Rene Yana Choque",
       horario: "15:00 - 16:00",
-      foto: rickRoberts,
+      foto: rene,
       descripFoto: "Foto del ponente RENE YANA CHOQUE"
     },
     {
       tituloPonencia: "Exploración De Predictores De La Satisfacción Con La Vida De Docentes Peruanos Mediante Un Ensamble De Métodos De Selección De Características",
       nombrePonente: "Luis Alberto Holgado Apaza",
       horario: "16:00 - 17:00",
-      foto: rickRoberts,
+      foto: holgado,
       descripFoto: "Foto del ponente LUIS ALBERTO HOLGADO APAZA"
     },
     {
       tituloPonencia: "( vacío )",
       nombrePonente: "Edy Larico Mamani",
       horario: "17:00 - 18:00",
-      foto: rickRoberts,
+      foto: edy,
       descripFoto: "Foto del ponente ROMARIO DIAZ HOLGADO"
     }
   ],
@@ -106,28 +118,28 @@ export const data = [
       tituloPonencia: "Inteligencia Artificial Generativa Para Creación De Contenido Visual En Visión Computacional",
       nombrePonente: "Ana Beatriz Alvarez Mamani",
       horario: "9:00 - 10:00",
-      foto: arlindoConceicao,
+      foto: ana,
       descripFoto: "Foto del ponente ANA BEATRIZ ALVAREZ MAMANI"
     },
     {
       tituloPonencia: "Aplicaciones De La Inteligencia Artificial En La Minería Con Operaciones De Tajo Abierto: Un Enfoque Sistémico",
       nombrePonente: "Norma Catacora Flores",
       horario: "10:00 - 11:00",
-      foto: danielAliaga,
+      foto: norma,
       descripFoto: "Foto del ponente NORMA CATACORA FLORES"
     },
     {
       tituloPonencia: "Teledetección Satelital Y Sus Aplicaciones",
       nombrePonente: "Luis José Díaz López",
       horario: "11:00 - 12:00",
-      foto: darioVieira,
+      foto: luis,
       descripFoto: "Foto del ponente LUIS JOSÉ DÍAZ LÓPEZ"
     },
     {
       tituloPonencia: "Modelos Generativos Multimodales",
       nombrePonente: "Cesar Armando Beltran Castañon",
       horario: "12:00 - 13:00",
-      foto: rickRoberts,
+      foto: cesar,
       descripFoto: "Foto del ponente CESAR ARMANDO BELTRAN CASTAÑON"
     }
   ]
