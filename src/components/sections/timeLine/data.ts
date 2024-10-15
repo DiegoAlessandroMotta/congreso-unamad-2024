65f5c053abc09a226d615e2b9232342efa98192e
yukio <zviamontevilca@gmail.com>
2024-10-15 16:43:28 -0500

    actualizando datos del cronograma
    datos del cronograma

--- src/components/sections/timeLine/data.ts
+++ src/components/sections/timeLine/data.ts
@@ -17,49 +17,64 @@ import cesar from "./assets/cesar.jpeg"
 import eduardo_zubieta from "./assets/Eduardo-Zubieta.png"
 import luisDiaz from "./assets/Luis-diaz.jpg"
 import humbertoFernandez from "./assets/Humberto-Fernandez-Calle.jpg"
+import LogoUnamad from "./assets/LogoUnamad.png"
 // LOS DATOS YA ESTÁN ACTUALIZADOS excepto la foto de cada ponente
 
 export const data = [
   [
     {
+      tituloPonencia: "Presentación de Eventos Artisticos",
+      nombrePonente: "Universidad Nacional Amazonica de Madre de Dios",
+      horario: "9:00 - 9:30",
+      foto: LogoUnamad,
+      descripFoto: "Logo de la UNAMAD"
+    },
+    {
+      tituloPonencia: "Palabras de Bienvenida e Inauguración ",
+      nombrePonente: "M.Sc. Yban Vilchez Navarro",
+      horario: "( 9:30 - 9:45 ) - ( 9:45 - 10:00 )  ",
+      foto: LogoUnamad,
+      descripFoto: "foto del ponente DIODOMIRO BALDOMERO LUQUE CARCASI"
+    },
+    {
       tituloPonencia: "Iot En El Contexto De Las Ciudades Inteligentes Y El Desarrollo Regional",
       nombrePonente: "Diodomiro Baldomero Luque Carcasi",
-      horario: "10:00 - 11:00",
+      horario: "10:00 - 10:50",
       foto: diodomiro,
       descripFoto: "foto del ponente DIODOMIRO BALDOMERO LUQUE CARCASI"
     },
     {
       tituloPonencia: "Contribución Del Aprendizaje Automático A La Resolución De Procesos Del Derecho Penal",
       nombrePonente: "Antonio Puma Choquehuanca",
-      horario: "11:00 - 12:00",
+      horario: "10:50 - 11:40",
       foto: marcoAntonio,
       descripFoto: "foto del ponente Antonio Puma Choquehuanca"
     },
     {
       tituloPonencia: "Ingeniería De Software: Tendencias De Las Metodologías Actuales",
       nombrePonente: "Dany Dorian Isuiza Perez",
-      horario: "12:00 - 13:00",
+      horario: "12:00 - 12:50",
       foto: dany,
       descripFoto: "foto del ponente DANY DORIAN ISUIZA PEREZ"
     },
     {
       tituloPonencia: "Infraestructura De Datos Espaciales y Catastro Único Regional",
       nombrePonente: "Jose Carlos Navarro Vega",
-      horario: "15:00 - 16:00",
+      horario: "15:00 - 15:50",
       foto: joseCarlos,
       descripFoto: "foto del ponente JOSE CARLOS NAVARRO VEGA"
     },
     {
       tituloPonencia: "Os Desafios Do Ensino, Pesquisa E Extensão Da Ciência Da Computação Na Amazônia",
       nombrePonente: "Anselmo Fortunato Ruiz Rodriguez",
-      horario: "16:00 - 17:00",
+      horario: "15:50 - 16:40",
       foto: anselmo,
       descripFoto: "foto del ponente ANSELMO FORTUNATO RUIZ RODRIGUEZ"
     },
     {
       tituloPonencia: "Modelo De Inteligencia Artificial Para La Detección De Emociones",
       nombrePonente: "Jaffet Sillo Sosa",
-      horario: "17:00 - 18:00",
+      horario: "16:40 - 17:30",
       foto: silloSosa,
       descripFoto: "foto del ponente JAFFET SILLO SOSA"
     }
@@ -67,57 +82,57 @@ export const data = [
   [
     {
       tituloPonencia: "Análisis Big Data En Los Procesos Académicos De La Universidad Amazónica De Pando (UAP)",
-      nombrePonente: "Juan Carlos Huanca Guanca",
-      horario: "9:00 - 10:00",
+      nombrePonente: "PhD. Juan Carlos Huanca Guanca",
+      horario: "9:00 - 9:50",
       foto: JuanCarlos,
       descripFoto: "Foto del ponente JUAN CARLOS HUANCA GUANCA"
     },
     {
       tituloPonencia: "Aplicaciones De La Inteligencia Artificial En La Minería Con Operaciones De Cielo Abierto: Un Enfoque Sistémico",
       nombrePonente: "Norma Catacora Flores",
-      horario: "10:00 - 11:00",
+      horario: " 9:50 - 10:40 ",
       foto: norma,
       descripFoto: "Foto del ponente Norma Catacora Flores"
     },
     {
       tituloPonencia: "Inteligencia Artificial Y Educación Universitaria",
-      nombrePonente: "Ponce Atencio Yalmar Temistocles",
-      horario: "11:00 - 12:00",
+      nombrePonente: "Dr. Ponce Atencio Yalmar Temistocles",
+      horario: "10:40 - 11:30",
       foto: ponce,
       descripFoto: "Foto del ponente PONCE ATENCIO YALMAR TEMISTOCLES"
     },
     {
       tituloPonencia: "Impacto De La Inteligencia Artificial En El Desarrollo De Software",
-      nombrePonente: "Romario Diaz Holgado",
-      horario: "12:00 - 13:00",
+      nombrePonente: "Ing. Romario Diaz Holgado",
+      horario: "11:50 - 12:40",
       foto: romario,
       descripFoto: "Foto del ponente ROMARIO DIAZ HOLGADO"
     },
     {
       tituloPonencia: "Integración De Herramientas De IA En El Proceso De Enseñanza-Aprendizaje En Ingeniería De Sistemas",
       nombrePonente: "Humberto Fernandez Calle",
-      horario: "15:00 - 16:00",
+      horario: "15:00 - 15:50",
       foto: humbertoFernandez,
       descripFoto: "Foto del ponente Humberto Fernandez Calle"
     },
     {
       tituloPonencia: "Exploración De Predictores De La Satisfacción Con La Vida De Docentes Peruanos Mediante Un Ensamble De Métodos De Selección De Características",
-      nombrePonente: "Luis Alberto Holgado Apaza",
-      horario: "16:00 - 17:00",
+      nombrePonente: "Dr. Luis Alberto Holgado Apaza",
+      horario: "15:50 - 16:40",
       foto: holgado,
       descripFoto: "Foto del ponente LUIS ALBERTO HOLGADO APAZA"
     },
     {
       tituloPonencia: "La Investigación En Ingeniería De Software",
       nombrePonente: "Edy Larico Mamani",
-      horario: "17:00 - 18:00",
+      horario: "16:40 - 17:30",
       foto: edy,
       descripFoto: "Foto del ponente Edy Larico Mamani"
     },
     {
       tituloPonencia: "Detección Del Celo En Vacuno De Carne Usando Iot Y Aprendizaje Automatico",
       nombrePonente: "Jaime César Prieto Luna",
-      horario: "18:00 - 19:00",
+      horario: "17:30 - 18:20",
       foto: jaime,
       descripFoto: "Foto del ponente Jaime César Prieto Luna"
     }
@@ -140,8 +155,29 @@ export const data = [
     {
       tituloPonencia: "Modelos Generativos Multimodales",
       nombrePonente: "Cesar Armando Beltran Castañon",
-      horario: "  :00 - 12:00",
+      horario: "  11:00 - 12:00",
       foto: cesar,
+      descripFoto: "Foto del ponente Cesar Armando Beltran Castañon"
+    },
+    {
+      tituloPonencia: "Presentacion de Eventos Artísticos",
+      nombrePonente: "Presentacion de Eventos Artísticos",
+      horario: "  12:00 - 12:30",
+      foto: LogoUnamad,
+      descripFoto: "Foto del ponente Cesar Armando Beltran Castañon"
+    },
+    {
+      tituloPonencia: "Palabras de Agradecimiento",
+      nombrePonente: "M.Sc. Yban Vilchez Navarro",
+      horario: "  12:30 - 12:45",
+      foto: LogoUnamad,
+      descripFoto: "Foto del ponente Cesar Armando Beltran Castañon"
+    },
+    {
+      tituloPonencia: "Palabras de Cierre ",
+      nombrePonente: "M.Sc. Yban Vilchez Navarro",
+      horario: "  12:45 - 13:00",
+      foto: LogoUnamad,
       descripFoto: "Foto del ponente Cesar Armando Beltran Castañon"
     }
   ]
